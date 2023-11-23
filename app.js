3148016ce45c119a118c861957e0a7baefd88cfc
Attapon Sroysuwan <55727960+eikqzx@users.noreply.github.com>
2023-11-23 12:10:45 +0700

    Q

--- app.js
+++ app.js
@@ -276,7 +276,7 @@ app.get('/api/file', async (req, res) => {
                 });
                 return;
             }
-            let desiredData = await sharp(data).jpeg({quality: 70}).toBuffer();
+            let desiredData = await sharp(data).jpeg({quality: 60}).toBuffer();
             const fileAsBase64 = desiredData.toString('base64');
             res.status(200).send({
                 status: true,
